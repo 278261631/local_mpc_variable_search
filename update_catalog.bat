f6ff033e8df8d6ef46d3415a82b54d5e6f85ab51
278261631 <mars.maya.g@gmail.com>
2026-03-13 17:32:31 +0800

    bat

--- update_catalog.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Update PyMPC asteroid catalog from Minor Planet Center
-
-echo Updating asteroid catalog from MPC...
-echo This may take several minutes...
-echo.
-
-python pympc_server/pympc_asteroid_server.py --update-only
-
